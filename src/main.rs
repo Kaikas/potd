b15acd55410e15513dc269fd051b153fa9e133f2
kaikas <max.riechelmann@googlemail.com>
2022-02-05 17:28:35 +0100

    static array

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 mod quote;
+
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
+    println!("{}", crate::quote::get_random_quote());
+}
+
+fn post() {
     let mut pastlines = Vec::<String>::new();
     let mut quotes = Vec::<String>::new();
     println!("mod quote {{");
     println!("pub fn get_random_quote() {{");
-    println!("let quotes = Vec::<String>::new()");
+    println!("let quotes = [");
     if let Ok(lines) = read_lines("./pqf") {
         let mut count = 0;
         for line in lines {
@@ -20,17 +25,15 @@ fn main() {
                     pastlines.push(l);
                 }
                 if count == 2 {
-                    quotes.push(pastlines.join("\n"));
+                    quotes.push(pastlines.join("\n "));
                     for x in &quotes {
-                        println!("quotes.push(\"{}\");", x.replace("\"", "''"));
+                        println!("\"{}\",", x.replace("\"", "'"));
                     }
                     pastlines.clear();
                 }
             }
         }
     }
-    println!("quotes.choose(&mut rand::thread:rng());");
-    println!("}}\n}}")
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
